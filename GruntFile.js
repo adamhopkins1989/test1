c924d896b660bf05e0a9f0693c702ce5bae879e2
adam89 <adamhopkins87@gmail.com>
2016-03-16 22:46:11 +0000

    updated grunt file with JS compilers

--- GruntFile.js
+++ GruntFile.js
@@ -2,84 +2,100 @@
  * Adams Gruntfile
  * @author Adam
  */
- 
+
 'use strict';
- 
+
 module.exports = function(grunt) {
 
-  grunt.initConfig({
+    grunt.initConfig({
 
-    // Project configuration
-    pkg: grunt.file.readJSON('package.json'),
+            // Project configuration
+            pkg: grunt.file.readJSON('package.json'),
 
-    // Compile Sass
-    sass: {
-      options: {
-        sourceMap: true,
-        sourceComments: false
-      },
-      dist: {
-        files: {
-          'dist/css/app.css': 'dev/scss/app.scss'
+            // Compile Sass
+            sass: {
+                options: {
+                    sourceMap: true,
+                    sourceComments: false
+                },
+                dist: {
+                    files: {
+                        'dist/css/app.css': 'dev/scss/app.scss'
+                    }
+                }
+            },
+
+            // Copy html files
+            copy: {
+                main: {
+                    files: [{
+                        expand: true,
+                        cwd: 'dev',
+                        src: ['**/*.html'],
+                        dest: 'dist'
+                    }]
+                }
+            },
+
+            concat: {
+                options: {
+                    separator: 'rn'
+                },
+                dev: {
+                    src: ['js/module1.js', 'js/module2.js'],
+                    dist: 'js/main.js'
+                }
+            }
+
+        }),
+
+        // Watch and build
+        watch: {
+            sass: {
+                files: 'dev/scss/*.scss',
+                tasks: ['sass']
+            },
+            html: {
+                files: ['dev/*.html'],
+                tasks: ['copy'],
+                options: {
+                    livereload: true
+                }
+            },
+        },
+
+        //Browersync
+        browserSync: {
+            default_options: {
+                bsFiles: {
+                    src: [
+                        "dist/css/*.css",
+                        "dist/*.html"
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: "./dist"
+                    }
+                }
+            }
         }
-      }
-    },
 
-    // Copy html files
-    copy: {
-        main: {
-            files: [{
-                expand: true,
-                cwd: 'dev',
-                src: ['**/*.html'],
-                dest: 'dist'
-            }]
-        }
-    },
+    });
 
-    // Watch and build
-    watch: {
-      sass: {
-        files: 'dev/scss/*.scss',
-        tasks: ['sass']
-      },
-      html: {
-        files: ['dev/*.html'],
-        tasks: ['copy'],
-        options: {
-        	livereload: true
-        }
-      },
-    },
+// Load dependencies
+grunt.loadNpmTasks('grunt-browser-sync');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-sass');
+grunt.loadNpmTasks('grunt-contrib-copy');
 
-    //Browersync
-   	browserSync: {
-   		default_options: {
-   			bsFiles: {
-   				src: [
-   				"dist/css/*.css",
-   				"dist/*.html"
-   				]
-   			},
-		    options: {
-		    	watchTask: true,
-		    	server: {
-		    		baseDir: "./dist"
-		    	}
-		    }
-		}
-	}
 
-  });
-
-  // Load dependencies
-  grunt.loadNpmTasks('grunt-browser-sync');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  
-
-  // Run tasks
-  grunt.registerTask('default', ['browserSync','sass','copy','watch']);
+// Run tasks
+grunt.registerTask(['browserSync', 'sass', 'jshint', 'concat','uglify','copy', 'watch']
+]);
 
 };
